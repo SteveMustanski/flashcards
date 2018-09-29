262f51aaf8f139e32be0b79e4822da59b0f92fc1
Steve Mustanski <steve.mustanski@gmail.com>
2018-09-29 16:17:19 -0400

    added /hello route, callback to listen, and returned html

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ const app = express();
 
 //create the root route
 // params 1. location, 2. callback
-app.get('/', (request, response) => {
-  response.send('This is the default route, so like hello world or something');
+app.get('/', (req, res) => {
+  res.send('<h1>This is the default route, so like hello world or something</h1>');
+});
+
+app.get('/hello', (req, res) => {
+  res.send('<h1>Hello JS developer!</h1>');
 });
 
 // start server listening on port 3000
-app.listen(3000);
+// params 1. port 2. callback
+app.listen(3000, () => {
+  console.log('The applicaiton is listening on port: 3000');
+});
 
