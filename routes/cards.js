2bcc4cc8ee4955a91a60e1513df71c3dec3b2bb0
Steve Mustanski <steve.mustanski@gmail.com>
2018-10-02 14:43:18 -0400

    added id to the querry paramater and route to select a card

--- routes/cards.js
+++ routes/cards.js
@@ -8,10 +8,10 @@ const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
 
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {
   res.render('card', {
-    prompt: cards[0].question,
-    hint: cards[0].hint
+    prompt: cards[req.params.id].question,
+    hint: cards[req.params.id].hint
   });
 });
 
